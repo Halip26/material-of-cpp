cb3e19e7696d1fa69db0e6e8db75ab4ba484f43a
Halip26 <halipuddin.angko@gmail.com>
2023-09-29 20:36:33 +0800

    add ternary operator project

---
+++ ternary-operator/project2.cpp
@@ -0,0 +1,21 @@
+//overspeeding evluation program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  double vehicle_speed;
+  double speed_limit;
+  
+  cout<<"Enter speed limit in your state: ";
+  cin>>speed_limit;
+  
+  cout << "Enter instantenous speed of the vehicle: ";
+  cin >> vehicle_speed;
+
+  string result = (vehicle_speed >= speed_limit) ? "You are overspeeding, a fine has been issued in your vehicle registration number!" : "Have a safe trip, sorry for inconvinience!";
+
+  cout << result;
+  
+  return 0;
+}
