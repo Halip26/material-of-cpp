3f7c1dfd4433c9be162c86f05a3feb66629b185a
Halip26 <halipuddin.angko@gmail.com>
2023-10-03 00:58:37 +0800

    refactor the codes

--- ternary-operator/project1.cpp
+++ ternary-operator/project1.cpp
@@ -10,15 +10,14 @@ int main() {
 
   cout << "--------------------------" << endl;
   cout << "This using Ternary operator: " << endl;
-  // ternary operator checks if, examValue is greater than 40
-  string result = (examValue >= 40) ? "passed" : "failed";
 
+  string result = (examValue >= 40) ? "passed" : "failed";
   cout << "You " << result << " the exam." << endl;
 
   cout << "--------------------------" << endl;
   cout << "This using if statement: " << endl;
-  // this used if else statement
-  if  (examValue >= 40) {
+
+  if (examValue >= 40) {
     cout << "You passed the exam." << endl;
   } else {
     cout << "You failed the exam." << endl;
