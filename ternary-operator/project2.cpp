6b3e4b9612ef9d137995e596eff02a6990d60953
Halip26 <halipuddin.angko@gmail.com>
2023-10-06 21:02:21 +0800

    change variable names to bahasa

--- ternary-operator/project2.cpp
+++ ternary-operator/project2.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int main() {
-  double vehicle_speed;
-  double speed_limit;
+  double kecepatanKendaraan;
+  double batasKecepatan;
   
-  cout << "Masukkan batas kecepatan di negara bagian Anda: ";
-  cin >> speed_limit;
+  cout << "Masukkan batas kecepatan di negara Indonesia: ";
+  cin >> batasKecepatan;
   
-  cout << "Memasukkan kecepatan seketika kendaraan: ";
-  cin >> vehicle_speed;
+  cout << "Memasukkan kecepatan kendaraan Kamu: ";
+  cin >> kecepatanKendaraan;
 
-  string result = (vehicle_speed >= speed_limit) ? "Anda melebihi batas kecepatan, denda telah dikeluarkan di STNK Anda!" : "Semoga perjalanan Anda aman, maaf atas ketidaknyamanannya!";
+  string result = (kecepatanKendaraan >= batasKecepatan) ? "Anda melebihi batas kecepatan, denda telah dikeluarkan di STNK Anda!" : "Semoga perjalanan Anda aman, maaf atas ketidaknyamanannya!";
 
   cout << result;
   
