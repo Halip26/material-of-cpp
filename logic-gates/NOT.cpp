753f2bf0624ff3f770ab63dd0e60f1d0ed70a942
Halip26 <halipuddin.angko@gmail.com>
2023-11-17 23:15:00 +0800

    add new prints

--- logic-gates/NOT.cpp
+++ logic-gates/NOT.cpp
@@ -1,16 +1,29 @@
-//Program to implement NOT Logic
+// Program to implement NOT Logic
 
 #include <iostream>
 using namespace std;
 
 int main() {
     int a = 7;
+    int b = 14;
 
     // output = true
     cout << !(a == 0) << endl;
 
     // output = false
     cout << !(a == 7) << endl;
+
+    // output = false
+    cout << !(b >= 14) << endl;
+
+    // output = true
+    cout << !(b <= 7) << endl;
+
+    // output = true
+    cout << !((a == 0) || (a > b)) << endl;
+    
+    // output = false
+    cout << ((a == 15) && !(a > b)) << endl;
 
     return 0;
 }
