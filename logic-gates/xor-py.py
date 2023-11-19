c77bdf1a48bf665df700a30c26a9d139791e39f1
Halip26 <halipuddin.angko@gmail.com>
2023-11-19 22:34:41 +0800

    add xor in python

---
+++ logic-gates/xor-py.py
@@ -0,0 +1,11 @@
+a = 15
+b = 19
+
+# Implementation logical operator XOR (^)
+print((a == 0) ^ (a > b))  # false
+
+print((a == 0) ^ (a < b))  # true
+
+print((a == 15) ^ (a > b))  # true
+
+print((a == 19) ^ (a < b))  # true
