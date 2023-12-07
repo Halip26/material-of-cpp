be7cb7d98e12793798a6070e808cb636466385f8
Halip26 <halipuddin.angko@gmail.com>
2023-12-07 20:33:28 +0800

    formatted

--- nested-conditionals/odd-even-positive-negative.cpp
+++ nested-conditionals/odd-even-positive-negative.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
   int number;
 
-  while(true){
+  while (true)
+  {
     cout << "Enter an integer: ";
     cin >> number;
 
-    if ((number > 0) && ((number % 2) == 0)) {
+    if ((number > 0) && ((number % 2) == 0))
+    {
       cout << "the number " << number << " is even positive." << endl;
-    } 
-    else  if ((number > 0) && ((number % 2 )>= 1)) {
+    }
+    else if ((number > 0) && ((number % 2) >= 1))
+    {
       cout << "the number " << number << " is odd positive." << endl;
-    } 
+    }
 
-    else if ((number < 0) && ((number % 2 ) == 0 )) {
+    else if ((number < 0) && ((number % 2) == 0))
+    {
       cout << "the number " << number << " is even negative." << endl;
-    } 
-    else if ((number < 0) && ((number % 2 ) <= 1)) {
+    }
+    else if ((number < 0) && ((number % 2) <= 1))
+    {
       cout << "the number " << number << " is odd negative." << endl;
     }
-    
-    else {
+
+    else
+    {
       cout << "Program is stop, you entered 0!" << endl;
       break;
     }
-
   }
-    
+
   return 0;
 }
--- nested-conditionals/project1.cpp
+++ nested-conditionals/project1.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
   int number;
 
   cout << "Enter an integer: ";
   cin >> number;
 
-  if (number % 2 == 0) {
+  if (number % 2 == 0)
+  {
     cout << "the number " << number << " is even.";
   }
-  else {
+  else
+  {
     cout << "the number " << number << " is odd.";
   }
   return 0;
-} 
+}
 
 // jadi pada kasus diatas, 2 kali berapa yang hasilnya mendekati/sama dengan nilai variabelnya
