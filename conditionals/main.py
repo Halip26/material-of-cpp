c655437d01801fff0907703a890a021720f5d2d8
Halip26 <halipuddin.angko@gmail.com>
2023-12-28 10:08:08 +0800

    modified & fixed it

--- conditionals/main.py
+++ conditionals/main.py
@@ -1,14 +1,44 @@
-username = "halip"
-password = "123456"
+import time
 
-enterUsername = str(input("Masukkan username kamu: "))
-enterPassword = str(input("Masukkan password kamu: "))
+username = "halip26"
+password = "halip123456"
+print(" ")
+username_input = str(input("Enter your username:"))
+password_input = str(input("Enter your password:"))
+confirm_password = str(input("Enter your password again:"))
 
-if username == enterUsername and password == enterPassword:
-    print("Anda berhasil log in, " + username)
-elif username != enterUsername and password == enterPassword:
-    print("Username anda tidak sama")
-elif username == enterUsername and password != enterPassword:
-    print("Password anda tidak sama")
+print("-" * 40)
+if (
+    username == username_input
+    and password == password_input
+    and confirm_password == password_input
+):
+    time.sleep(1)
+    print("Hii, Welcome to your account", username)
+elif (
+    username != username_input
+    and password == password_input
+    and confirm_password == password_input
+):
+    time.sleep(1)
+    print("Wrong username, Try again!")
+elif (
+    username == username_input
+    and password != password_input
+    and confirm_password == password_input
+):
+    time.sleep(1)
+    print("Wrong password, Try again!")
+elif password_input != confirm_password:
+    time.sleep(1)
+    print("Confirm password doesn't match, Try again!")
 else:
-    print("Username & Password tidak ada")
+    time.sleep(1)
+    print(
+        "Username:",
+        username_input,
+        "&",
+        "Password:",
+        password_input,
+        "Are not exists, Try again!",
+    )
