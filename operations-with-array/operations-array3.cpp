8d61d564f72963745839bf7fcac1399f1df9758d
Halip26 <halipuddin.angko@gmail.com>
2024-01-01 20:30:22 +0800

    update armstrong number pj

--- operations-with-array/operations-array3.cpp
+++ operations-with-array/operations-array3.cpp
@@ -5,35 +5,40 @@ For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.*/
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  int a[100], i, n;
+  int numbers[100], n;
 
-  cout << "Enter how many numbers you want to check for armstrong propperty\n";
+  cout << "Enter how many numbers you want to check for the Armstrong property\n";
   cin >> n;
 
-  cout << "Enter the element: ";
-  for (i = 0; i < n; i++)
+  cout << "Enter the elements:\n";
+  for (int i = 0; i < n; i++)
   {
-    cin >> a[i];
+    cin >> numbers[i];
   }
 
-  for (i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
   {
-    int k = 0, rem = 0, sum = 0;
+    int originalNumber = numbers[i];
+    int digit, sum = 0;
 
-    k = a[i];
-    while (k != 0)
-    {                              // untill last digit of the number reached
-      rem = k % 10;                // getting the digit of the numbers
-      sum = sum + rem * rem * rem; // adding sum and cube of the digit
-      k = k / 10;                  // getting next number to get next digit
+    while (originalNumber != 0)
+    {
+      digit = originalNumber % 10;
+      sum += digit * digit * digit;
+      originalNumber /= 10;
     }
 
-    if (sum == a[i]) // if calculated sum is equal to the number provided
-      cout << a[i] << "\t is an ArmStrong number\n\n";
+    if (sum == numbers[i])
+    {
+      cout << numbers[i] << " is an Armstrong number\n\n";
+    }
     else
-      cout << a[i] << "\t is not an ArmStrong number\n\n";
+    {
+      cout << numbers[i] << " is not an Armstrong number\n\n";
+    }
   }
 
   return 0;
