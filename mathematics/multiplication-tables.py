b1dfc1b03945271d755437681da4d6d52ecb4668
Halip26 <halipuddin.angko@gmail.com>
2024-01-18 21:36:00 +0800

    update & fix it

--- mathematics/multiplication-tables.py
+++ mathematics/multiplication-tables.py
@@ -4,3 +4,13 @@ i = 1
 while i <= 10:
     print(i, "x", num, "=", num * i)
     i += 1
+
+"""
+# multiplication table using for
+number = int(input("Enter the number: "))
+
+print(f"\nMultiplication Table for {number}:\n")
+for i in range(1, 11):
+    print(f"{i} x {number}  = {number * i}")
+    i += 1
+"""
