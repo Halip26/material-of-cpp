d083c56c86902b66fa166a3261d435bf44790360
Halip26 <halipuddin.angko@gmail.com>
2024-01-31 22:30:48 +0800

    update & fix it

--- flow-control/for-loop/for-loop3.cpp
+++ flow-control/for-loop/for-loop3.cpp
@@ -9,12 +9,34 @@ int main()
   int row;
   cin >> row;
 
+  cout << "\nCetak stairs pattern: " << endl;
+
+  // create stairs pattern
   for (int r = 1; r <= row; r++)
   {
     for (int c = 1; c <= r; c++)
     {
       cout << " * ";
     }
+    cout << endl;
+  }
+
+  cout << "\nCetak triangle pattern: " << endl;
+  // triangle pattern
+  for (int s = 1; s <= row; s++)
+  {
+    // cetak spasi sebelum karakter bintang
+    for (int t = 1; t <= (row - s); t++)
+    {
+      cout << "  ";
+    }
+
+    // cetak karakter bintang
+    for (int u = 1; u <= 2 * s - 1; u++)
+    {
+      cout << "* ";
+    }
+
     cout << endl;
   }
 
