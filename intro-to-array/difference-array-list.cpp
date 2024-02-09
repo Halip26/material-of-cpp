e078a6b42eceeea68bbb12c9402aab9318f5acae
Halip26 <halipuddin.angko@gmail.com>
2024-02-09 22:32:54 +0800

    add list in int

--- intro-to-array/difference-array-list.cpp
+++ intro-to-array/difference-array-list.cpp
@@ -7,35 +7,52 @@ using namespace std;
 int main()
 {
   // deklarasi array
-  char nama_siswa[10][20];
+  string nama_siswa[5];
+
   // inisialisasi array
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 5; i++)
   {
     cout << "Masukkan nama siswa ke-" << i + 1 << ": ";
     cin >> nama_siswa[i];
   }
+  cout << "-------------------------------------" << endl;
 
   // mencetak array
-  cout << "mencetak nama2 siswa yg telah diinput pada array" << endl;
-  for (int i = 0; i < 10; i++)
+  cout << "mencetak nama2 siswa yg telah diinput pada array: " << endl;
+  for (int i = 0; i < 5; i++)
   {
     cout << "Nama siswa ke-" << i + 1 << ": " << nama_siswa[i] << endl;
   }
 
   // deklarasi list
   list<string> daftar_belanja;
+  list<int> jumlah;
 
   // menambah elemen ke list
   daftar_belanja.push_back("Beras");
   daftar_belanja.push_back("Gula");
   daftar_belanja.push_back("Tepung");
+  jumlah.push_back(3);
+  jumlah.push_back(10);
+  jumlah.push_back(15);
 
   cout << "-------------------------------------" << endl;
   cout << "Contoh hasil dari tipe data collection list" << endl;
-  // mencetak list
+
+  // mencetak list daftar_belanja
+  cout << "1. Mencetak list daftar_belanja: " << endl;
   for (const string &item : daftar_belanja)
   {
-    cout << item << " ";
+    cout << "   ";
+    cout << "- " << item << endl;
+  }
+  cout << endl;
+  // mencetak list jumlah
+  cout << "2. Mencetak list jumlah: " << endl;
+  for (const int &item : jumlah)
+  {
+    cout << "   ";
+    cout << "- " << item << endl;
   }
 
   return 0;
