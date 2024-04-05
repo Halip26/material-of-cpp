9ddab0b3557ba6163ab807170efd02cd927927d5
Halip26 <halipuddin.angko@gmail.com>
2024-04-05 22:59:28 +0800

    refactor the code & add gitignore

--- files/project1.cpp
+++ files/project1.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <string> // Tambahkan library string untuk menggunakan tipe data string
 
 using namespace std;
 
+struct Student
+{ // Buat struct untuk merepresentasikan data mahasiswa
+  string name;
+  float marks;
+};
+
 int main()
 {
-  ofstream fout("student");
-  char name[30], ch;
-  float marks = 0.0;
+  ofstream fout("student.txt"); // Ganti nama file ke "student.txt" agar lebih deskriptif
+  char ch;
 
+  // Array untuk menyimpan data mahasiswa
+  Student students[5];
+
+  // Input data dari pengguna
   for (int i = 0; i < 5; i++)
   {
     cout << i + 1 << ". Name :\t";
-    cin.get(name, 30);
+    cin >> students[i].name; // Ubah ke input string agar lebih aman
     cout << i + 1 << ". Marks: \t";
-    cin >> marks;
-    cin.get(ch);
+    cin >> students[i].marks;
+    cin.ignore(); // Hapus karakter newline dari buffer
+  }
 
-    fout << name << '\n'
-         << marks << '\n';
+  // Menulis data mahasiswa ke dalam file
+  for (int i = 0; i < 5; i++)
+  {
+    fout << students[i].name << '\n'
+         << students[i].marks << '\n';
   }
   fout.close();
 
-  ifstream fin("student");
-  fin.seekg(0);
-  cout << endl;
+  ifstream fin("student.txt");
 
+  // Membaca dan menampilkan data mahasiswa dari file
   for (int j = 0; j < 5; j++)
   {
-    fin.get(name, 30);
-    fin.get(ch);
-    fin >> marks;
-    fin.get(ch);
-    cout << j + 1 << ". Student name:\t" << name;
-    cout << "\t Marks:" << marks << '\n';
+    getline(fin, students[j].name); // Menggunakan getline untuk membaca string
+    fin >> students[j].marks;
+    fin.ignore(); // Hapus karakter newline dari buffer
+    cout << j + 1 << ". Student name:\t" << students[j].name;
+    cout << "\t Marks:" << students[j].marks << '\n';
   }
   fin.close();
+
+  return 0;
 }
