342de4f078c317af90cc446e4ff9d251d0433e89
Halip26 <halipuddin.angko@gmail.com>
2024-04-26 22:35:09 +0800

    add new example in oop

---
+++ object-oriented-programming/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Segitiga
+{
+private:
+  double alas;
+  double tinggi;
+
+public:
+  Segitiga(double a, double b)
+  {
+    this->alas = a;
+    this->tinggi = b;
+  }
+
+  double hitungLuas()
+  {
+    return (alas * tinggi) / 2;
+  }
+};
+
+int main()
+{
+  int alas, tinggi;
+
+  cout << "Masukkan alasnya: ";
+  cin >> alas;
+  cout << "Masukkan tingginya: ";
+  cin >> tinggi;
+
+  for (int i = 0; i <= 40; i++)
+  {
+    cout << "-";
+  }
+  cout << endl;
+
+  Segitiga segitiga(alas, tinggi);
+  double luas = segitiga.hitungLuas();
+  cout << "Luas segitiga: " << luas << endl;
+
+  return 0;
+}
