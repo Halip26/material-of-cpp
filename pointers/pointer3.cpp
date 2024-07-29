605dae6bde1d37ff2aa5d600b3a7b9eb71e1ed12
Halip26 <halipuddin.angko@gmail.com>
2024-07-29 20:18:39 +0800

    update the pointer symbol

--- pointers/pointer3.cpp
+++ pointers/pointer3.cpp
@@ -13,12 +13,12 @@ int main()
   // take the address of var
   ptr = &var;
 
-  // take the address of ptr using address of operator &
+  // take the address of ptr using address of operator "&"
   pptr = &ptr;
 
   // take the value using pptr
   cout << "Value of var :" << var << endl;
-  cout << "Value available at *ptr :" << *ptr << endl;
+  cout << "Value available at *ptr :" << &ptr << endl;
   cout << "Value available at **pptr :" << **pptr << endl;
 
   return 0;
