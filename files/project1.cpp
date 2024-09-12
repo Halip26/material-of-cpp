d4405ff41efe05551411be42d46a96ca9a871606
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-09-12 19:26:00 +0800

    change the name

--- files/project1.cpp
+++ files/project1.cpp
@@ -1,73 +1,42 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
-struct Student
-{
-  string name;
-  float marks;
-};
-
 int main()
 {
-  ofstream fout("student.txt");
-  char ch;
-  // Variabel untuk menyimpan jumlah siswa yang ingin dimasukkan
-  int numStudents;
+  fstream fio;
 
-  // buat garis doang
-  for (int i = 0; i <= 35; i++)
+  string line;
+
+  fio.open("project2.txt", ios::trunc | ios::out | ios::in);
+
+  cout << "\nEnter your text for file. To stop, type -1 in new line\n";
+  cout << "******************************************************\n";
+  while (fio)
   {
-    cout << "-";
-  }
-  cout << endl;
 
-  // meminta input jumlah siswanya
-  cout << "Enter the number of students: ";
-  cin >> numStudents;
+    getline(cin, line);
 
-  // Array untuk menyimpan data mahasiswa sesuai jumlah yang dimasukkan pengguna
-  Student students[numStudents];
+    if (line == "-1")
+      break;
 
-  // Input data dari pengguna
-  for (int i = 0; i < numStudents; i++)
-  {
-    cout << i + 1 << ". Name :\t";
-    cin >> students[i].name;
-    cout << i + 1 << ". Marks: \t";
-    cin >> students[i].marks;
-    cin.ignore(); // Hapus karakter newline dari buffer
+    fio << line << endl;
   }
 
-  // Menulis data mahasiswa ke dalam file
-  for (int i = 0; i < numStudents; i++)
+  fio.seekg(0, ios::beg);
+  cout << "\nReading from the file\n";
+  cout << "*********************\n";
+  while (fio)
   {
-    fout << students[i].name << '\n'
-         << students[i].marks << '\n';
-  }
-  fout.close();
 
-  ifstream fin("student.txt");
-
-  // buat garis doang
-  for (int i = 0; i <= 35; i++)
-  {
-    cout << "-";
+    getline(fio, line);
+    cout << line << endl;
   }
-  cout << endl;
 
-  // Membaca dan menampilkan data mahasiswa dari file
-  for (int j = 0; j < numStudents; j++)
-  {
-    getline(fin, students[j].name);
-    fin >> students[j].marks;
-    fin.ignore(); // Hapus karakter newline dari buffer
-    cout << j + 1 << ". Student name:" << students[j].name;
-    cout << "\t Marks: " << students[j].marks << '\n';
-  }
-  fin.close();
+  cout << "If you want to check the changes, you can download the file and see the change in it also.";
+
+  fio.close();
 
   return 0;
 }
