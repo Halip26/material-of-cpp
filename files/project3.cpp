56a418487f22824d13feac74a9568c6862621227
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-09-18 20:31:42 +0800

    modified the syntax

--- files/project3.cpp
+++ files/project3.cpp
@@ -34,9 +34,9 @@ int main()
   // Input data dari pengguna
   for (int i = 0; i < numStudents; i++)
   {
-    cout << i + 1 << ". Name :\t";
+    cout << i + 1 << ". Name : ";
     cin >> students[i].name;
-    cout << i + 1 << ". Marks: \t";
+    cout << i + 1 << ". Marks: ";
     cin >> students[i].marks;
     cin.ignore(); // Hapus karakter newline dari buffer
   }
@@ -44,8 +44,8 @@ int main()
   // Menulis data mahasiswa ke dalam file
   for (int i = 0; i < numStudents; i++)
   {
-    fout << students[i].name << '\n'
-         << students[i].marks << '\n';
+    fout << "Student name: " << students[i].name << '\n'
+         << "Student " << students[i].marks << '\n';
   }
   fout.close();
 
@@ -64,7 +64,7 @@ int main()
     getline(fin, students[j].name);
     fin >> students[j].marks;
     fin.ignore(); // Hapus karakter newline dari buffer
-    cout << j + 1 << ". Student name:" << students[j].name;
+    cout << j + 1 << ". Student name: " << students[j].name;
     cout << "\t Marks: " << students[j].marks << '\n';
   }
   fin.close();
