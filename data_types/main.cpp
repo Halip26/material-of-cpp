342b1257e900acadabe0cefe059efe041fde2dcf
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-11-17 20:35:04 +0800

    reformant the script

--- data_types/main.cpp
+++ data_types/main.cpp
@@ -2,23 +2,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
- 
-int main () {
+
+int main()
+{
   // Creating variables
-  int myNum = 5;               // Integer (whole number)
-  float myFloatNum = 5.99;     // Floating point number
-  double myDoubleNum = 9.98;   // Floating point number
-  char myLetter = 'D';         // Character
-  bool myBoolean = false;      // Boolean
-  string myString = "Hello, guys";   // String
-   
+  int myNum = 26;
+  long myLongNum = 30000;
+  float myFloatNum = 26.1;
+  double myDoubleNum = 26778.9;
+  char myLetter = 'H';
+  bool myBoolean = true;
+  string myString = "Hello, students!";
+
   // Print variable values
-  cout << "int: " << myNum << "\t\t\t" << "Size of Int: " << sizeof(myNum)<< " bytes" << "\n";
-  cout << "float: " << myFloatNum << "\t\t"<< "Size of Float: " << sizeof(myFloatNum)<< " bytes"<< "\n";
-  cout << "double: " << myDoubleNum << "\t"<< "Size of Double: " << sizeof(myDoubleNum)<< " bytes"<<"\n";
-  cout << "char: " << myLetter << "\t\t\t" << "Size of Character: " << sizeof(myLetter)<< " bytes"<< "\n";
-  cout << "bool: " << myBoolean << "\t\t\t" << "Size of Boolean: " << sizeof(myBoolean)<< " bytes" << "\n";
-  cout << "string: " << myString << "\t" << "Size of String: " << sizeof(myString)<< " bytes" << "\n";
- 
+  cout << "int: " << myNum << "\t\t\t" << "Size of Int: " << sizeof(myNum) << " bytes" << endl;
+  cout << "float: " << myFloatNum << "\t\t" << "Size of Float: " << sizeof(myFloatNum) << " bytes" << endl;
+  cout << "double: " << myDoubleNum << "\t" << "Size of Double: " << sizeof(myDoubleNum) << " bytes" << endl;
+  cout << "char: " << myLetter << "\t\t\t" << "Size of Character: " << sizeof(myLetter) << " bytes" << endl;
+  cout << "bool: " << myBoolean << "\t\t\t" << "Size of Boolean: " << sizeof(myBoolean) << " bytes" << endl;
+  cout << "string: " << myString << "\t" << "Size of String: " << sizeof(myString) << " bytes" << endl;
+
   return 0;
 }
