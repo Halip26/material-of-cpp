ed9340cf6581f1efb68ae3920a2d497039cd2f7f
Halip26 <77138807+Halip26@users.noreply.github.com>
2025-01-19 21:10:47 +0800

    add new C++ programs for variable declaration and basic input/output

---
+++ basic-cpp/declaration-definition.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // ini adalah declaration
+    int umur;
+
+    cout << "Print variable deklarasi: " << umur << endl;
+
+    // ini initialize
+    umur = 29;
+
+    // print varible hasil initialisasi
+    cout << "Umur Mr. Halip is " << umur << endl;
+
+    // ini initialize
+    umur = 16;
+
+    // print varible hasil initialisasi
+    cout << "Umur kris is " << umur << endl;
+
+    // next buat definition dengan nama hobby
+    string hobby = "main game";
+
+    cout << "Mr. Halip dan Kris hobby " << hobby << endl;
+
+    return 0;
+}
---
+++ basic-cpp/intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string nama = "Halipuddin";
+    int a = 56;
+    int b = 76;
+
+    std::cout << "Nama saya " << nama << std::endl;
+    std::cout << "Saya orang Makassar, Sulawesi selatan" << std::endl;
+
+    std::cout << "Hasil dari a + b adalah " << a + b << std::endl;
+
+    return 0;
+}
